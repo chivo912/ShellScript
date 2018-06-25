aeff19af3f36a052de1299f69e4a426c07a4aaec
Chí Võ <c.kid2k11@gmail.com>
2018-06-25 16:58:44 +0700

    check domain has already and add color

--- add-domain.sh
+++ add-domain.sh
@@ -7,17 +7,6 @@
 # Version 1.0.2
 # Login Root First
 # Usage : ./add-domain.sh chivo.org /home/chivo/public_html/
-echo
-echo '#################################################################'
-echo '# Script automatic add domain Apache2			     	#'
-echo '# Date: 05-12-2017						#'
-echo '# Script Author : Chi Vo					#'
-echo '# Homepage : www.chivo.org					#'
-echo '# Version 1.0.3							#'
-echo '# Login Root First						#'
-echo '# Usage : ./add-domain.sh chivo.org /home/chivo/public_html/	#'
-echo '#################################################################'
-echo
 
 #Check root
 user=$(whoami)
@@ -27,21 +16,44 @@ if [ ! "$user" = 'root' ]; then
 	exit
 fi
 
+# Color
+LIGHTRED='\033[1;31m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+LIGHTGREEN='\033[1;32m'
+NC='\033[0m'
+
+echo
+echo '#################################################################'
+echo '# Script automatic add domain Apache2			     	#'
+echo '# Date: 05-12-2017						#'
+echo '# Script Author : Chi Vo					#'
+echo '# Homepage : www.chivo.org					#'
+echo '# Version 1.1.0							#'
+echo '# Login Root First						#'
+echo '# Usage : ./add-domain.sh chivo.org /home/chivo/public_html/	#'
+echo '#################################################################'
+echo
+
 #Get parameters
 domain=$1
 path=$2
-if [ "$domain" ]; then
-	echo $domain
-else
+if [ ! "$domain" ]; then
 	echo "Input your domain : "
 	read domain
 fi
 
-if [ "$path" ]; then
-	echo $path
-else
+if [ ! "$path" ]; then
 	echo "Input your path : "
 	read path
+fi
+
+# Check domain
+check_domain_existed=$(grep -c "$domain" /etc/hosts)
+
+if [ "$check_domain_existed" != 0 ] ; then
+	echo "${RED}Domain ${LIGHTRED}$domain${RED} has already !\n"
+	exit
 fi
 
 #Run
@@ -61,4 +73,4 @@ echo '</VirtualHost>' >> /etc/apache2/sites-available/$domain'.conf'
 echo '# vim: syntax=apache ts=4 sw=4 sts=4 sr noet' >> /etc/apache2/sites-available/$domain'.conf'
 a2ensite $domain
 service apache2 restart
-
+echo "${GREEN}Domain ${LIGHTGREEN}$domain${GREEN} has been installed !\n"
